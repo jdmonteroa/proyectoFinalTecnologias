a760a0e76e44ad96b8f4e1c186fbd214315fbd7f
merenganov <webcraftbv@gmail.com>
2025-06-18 19:18:41 -0600

    firstcommit4

---
+++ src/enviroments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false, // o true según el entorno
+  firebase: {
+    apiKey: "AIzaSyBQNEZMHNrvwoxI8qE8qplA0el_jBdj4rs",
+    authDomain: "hotelbdd.firebaseapp.com",
+    projectId: "hotelbdd",
+    storageBucket: "hotelbdd.firebasestorage.app",
+    messagingSenderId: "314494788454",
+    appId: "1:314494788454:web:52f1a3839837dada571ea7",
+    measurementId: "G-QJ6CK1FZDL"
+  }
+};

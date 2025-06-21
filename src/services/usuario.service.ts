ee409b276c71f9ff07a13c485d75ad7a3ec73ab8
Jaretzi.sys <al335300@edu.uaa.mx>
2025-06-21 14:02:24 -0600

    Implementacion del form para registrar y el servicio de usuarios

---
+++ src/services/usuario.service.ts
@@ -0,0 +1,27 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+export interface Usuario {
+  id?: string;
+  username: string;
+  email: string;
+  password: string;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UsuarioService {
+  private apiUrl = 'http://localhost:3000/usuarios';
+
+  constructor(private http: HttpClient) { }
+
+  registrarUsuario(usuario: Usuario): Observable<any> {
+    return this.http.post(this.apiUrl, usuario);
+  }
+
+  obtenerUsuarios(): Observable<Usuario[]> {
+    return this.http.get<Usuario[]>(this.apiUrl);
+  }
+}

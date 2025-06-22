1d8a03dbe61904a03fd116e8876ad801a8f43c5b
merenganov <webcraftbv@gmail.com>
2025-06-22 11:48:44 -0600

    ODLCG: Ocultar componentes <video> y <contacto> según sesión activa
    Se agrego Auth.service para ocultar el video y contacto si no hay sesión activa

--- src/app/components/vistaAcerca/acerca/acerca.component.ts
+++ src/app/components/vistaAcerca/acerca/acerca.component.ts
@@ -1,15 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ContactoComponent } from '../contacto/contacto.component';
 import { CardsVisionComponent } from '../cards-vision/cards-vision.component';
 import { VideoComponent } from '../video/video.component';
+import { AuthService } from '../../../services/auth.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-acerca',
-  imports: [RouterOutlet,ContactoComponent,CardsVisionComponent, VideoComponent],
+  imports: [RouterOutlet,ContactoComponent,CardsVisionComponent, VideoComponent, CommonModule],
   templateUrl: './acerca.component.html',
   styleUrl: './acerca.component.css'
 })
 export class AcercaComponent {
+    isLoggedIn = false; // new property to track login status
 
+  constructor(private authService: AuthService) {}
+
+  ngOnInit(): void {
+    this.isLoggedIn = this.authService.isLoggedIn();
+  }
 }

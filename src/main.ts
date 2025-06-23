143a9a3eee305ffa9c76ae33745ffd202429fbd5
jdmonteroa <al347581@edu.uaa.mx>
2025-06-23 10:34:37 -0600

    Cambios antes de actualizar Angular

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,19 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
-import { provideHttpClient } from '@angular/common/http';  // Importar provideHttpClient
+import { provideHttpClient } from '@angular/common/http';
+import { isDevMode } from '@angular/core';
+import { provideServiceWorker } from '@angular/service-worker';  // Importar provideHttpClient
 
 // Modificar appConfig para agregar el proveedor de HttpClient
 const customAppConfig = {
   ...appConfig,
   providers: [
     ...appConfig.providers,
-    provideHttpClient()  // Agregar el proveedor de HttpClient
+    provideHttpClient(), provideServiceWorker('ngsw-worker.js', {
+            enabled: !isDevMode(),
+            registrationStrategy: 'registerWhenStable:30000'
+          })  // Agregar el proveedor de HttpClient
   ]
 };
 
